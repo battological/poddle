f612eb90c474157865625715bd8cade4c29f29b2
Garrick Sherman <backwardstg@gmail.com>
2016-11-05 19:51:08 -0700

    better pep8 adherence

--- test/tests.py
+++ test/tests.py
@@ -1,15 +1,15 @@
-import sys, os.path
+import os.path
+import sys
 
 from webtest import TestApp
 
 sys.path.append(os.path.abspath(os.path.join(
-	os.path.dirname(__file__),
-	os.path.pardir,
-	'src')))
+    os.path.dirname(__file__),
+    os.path.pardir,
+    'src')))
 
 from api import app
 from models import User, Podcast
-
 
 # Verbs
 TEST = 'Testing'
@@ -32,202 +32,199 @@ user_password_edited = 'edited123'
 # Test podcast details
 podcast_link = "http://test.com/feed"
 
-def testing(url, verb=None):
-	if verb is None:
-		verb = TEST
 
-	print('{} {}'.format(verb, url))
+def testing(url_, verb=None):
+    if verb is None:
+        verb = TEST
 
-	return url
+    print('{} {}'.format(verb, url_))
 
-def standard_test(res):
-	assert res.content_type == 'application/json'
-	assert res.content_length > 0
+    return url_
+
+
+def standard_test(result):
+    assert result.content_type == 'application/json'
+    assert result.content_length > 0
+
 
 def clear_obj(db_class, prop_name, prop_value):
-	obj = db_class.select().where(getattr(db_class, prop_name) == prop_value)
-	if obj.exists():
-		obj.get().delete_instance()
+    obj = db_class.select().where(getattr(db_class, prop_name) == prop_value)
+    if obj.exists():
+        obj.get().delete_instance()
 
 
 if __name__ == '__main__':
+    app = TestApp(app)
 
-	app = TestApp(app)
+    '''
+    Start by clearing out the test data.
+    With on delete cascade turned on, this should automatically clear
+       all testing data.
+    '''
+    clear_obj(User, 'email', user_email)
+    clear_obj(User, 'email', altuser_email)
+    clear_obj(User, 'email', user_email_edited)
+    clear_obj(Podcast, 'link', podcast_link)
 
+    # USER REGISTRATION #
+    url = testing('/api/user/register', POST)
 
-	'''
-	Start by clearing out the test data.
-	With on delete cascade turned on, this should automatically clear
-	   all testing data.
-	'''
-	clear_obj(User, 'email', user_email)
-	clear_obj(User, 'email', altuser_email)
-	clear_obj(User, 'email', user_email_edited)
-	clear_obj(Podcast, 'link', podcast_link)
+    # (X) Missing name
+    app.post_json(url, {"email": user_email,
+                        "password": user_password},
+                  status=400)
 
+    # (X) Missing password
+    app.post_json(url, {"email": user_email,
+                        "name": user_name},
+                  status=400)
 
-	### USER REGISTRATION ###
-	url = testing('/api/user/register', POST)
+    # (X) Missing email
+    app.post_json(url, {"password": user_password,
+                        "name": user_name},
+                  status=400)
 
-	# (X) Missing name
-	app.post_json(url, {"email": user_email,
-		"password": user_password},
-		status=400)
+    # (X) Password is only letters
+    app.post_json(url, {"email": user_email,
+                        "password": "password",
+                        "name": user_name},
+                  status=400)
 
-	# (X) Missing password
-	app.post_json(url, {"email": user_email,
-		"name": user_name},
-		status=400)
+    # (X) Password is less than 8 characters
+    app.post_json(url, {"email": user_email,
+                        "password": "pass",
+                        "name": user_name},
+                  status=400)
 
-	# (X) Missing email
-	app.post_json(url, {"password": user_password,
-		"name": user_name},
-		status=400)
+    # (*) All login data correct
+    res = app.post_json(url, {"email": user_email,
+                              "password": user_password,
+                              "name": user_name})
+    userId = res.json['id']
 
-	# (X) Password is only letters
-	app.post_json(url, {"email": user_email,
-		"password": "password",
-		"name": user_name},
-		status=400)
+    # (X) User already exists
+    app.post_json(url, {"email": user_email,
+                        "password": user_password,
+                        "name": user_name},
+                  status=409)
 
-	# (X) Password is less than 8 characters
-	app.post_json(url, {"email": user_email,
-		"password": "pass",
-		"name": user_name},
-		status=400)
+    # (-) Create an alternate user for testing purposes
+    res = app.post_json(url, {"email": altuser_email,
+                              "password": altuser_password,
+                              "name": altuser_name})
+    altUserId = res.json['id']
 
-	# (*) All login data correct
-	res = app.post_json(url, {"email": user_email,
-		"password": user_password,
-		"name": user_name})
-	userId = res.json['id']
+    # USER LOGIN #
+    url = testing('/api/user/login', POST)
 
-	# (X) User already exists
-	app.post_json(url, {"email": user_email,
-		"password": user_password,
-		"name": user_name},
-		status=409)
+    # (*) Successful login
+    res = app.post_json(url, {"email": user_email, "password": user_password})
+    standard_test(res)
 
-	# (-) Create an alternate user for testing purposes
-	res = app.post_json(url, {"email": altuser_email,
-		"password": altuser_password,
-		"name": altuser_name})
-	altUserId = res.json['id']
+    # (*) User ID matches the one supplied at register
+    assert res.json['id'] == userId
 
+    # (*) Collect the auth token
+    token = res.json['jwt']
+    assert len(token) > 0
+    auth = {'Authorization': 'Bearer {}'.format(token)}
 
-	### USER LOGIN ###
-	url = testing('/api/user/login', POST)
+    # (-) Collect the auth token for the alternate user
+    res = app.post_json(url, {"email": altuser_email, "password": altuser_password})
+    token = res.json['jwt']
+    altAuth = {'Authorization': 'Bearer {}'.format(token)}
 
-	# (*) Successful login
-	res = app.post_json(url, {"email": user_email, "password": user_password})
-	standard_test(res)
+    # (X) User does not exist
+    app.post_json(url,
+                  {"email": "wrong@nowhere.com", "password": user_password},
+                  status=401)
 
-	# (*) User ID matches the one supplied at register
-	assert res.json['id'] == userId
+    # (X) Incorrect password for existing user
+    app.post_json(url,
+                  {"email": user_email, "password": "wrongpassword"},
+                  status=401)
 
-	# (*) Collect the auth token
-	token = res.json['jwt']
-	assert len(token) > 0
-	auth = {'Authorization': 'Bearer {}'.format(token)}
+    # USER INFO #
+    url = testing('/api/user/{}'.format(userId), GET)
 
-	# (-) Collect the auth token for the alternate user
-	res = app.post_json(url, {"email": altuser_email, "password": altuser_password})
-	token = res.json['jwt']
-	altAuth = {'Authorization': 'Bearer {}'.format(token)}
+    # (*) Correct information
+    res = app.get(url)
+    standard_test(res)
+    assert res.json['id'] == userId
+    assert res.json['name'] == 'Tester'
 
-	# (X) User does not exist
-	app.post_json(url,
-		{"email": "wrong@nowhere.com", "password": user_password},
-		status=401)
-	
-	# (X) Incorrect password for existing user
-	app.post_json(url,
-		{"email": user_email, "password": "wrongpassword"},
-		status=401)
+    # (*) Password should not be visible
+    assert 'password' not in res.json
 
+    # (*) Auth token is correctly ignored, correct information
+    res = app.get(url, headers=auth)
+    standard_test(res)
+    assert res.json['id'] == userId
+    assert res.json['name'] == 'Tester'
 
-	### USER INFO ###
-	url = testing('/api/user/{}'.format(userId), GET)
+    # (X) User does not exist
+    url = testing('/api/user/{}'.format(5000), GET)
+    res = app.get(url, status=404)
+    url = testing('/api/user/{}'.format(-1), GET)
+    res = app.get(url, status=404)
 
-	# (*) Correct information
-	res = app.get(url)
-	standard_test(res)
-	assert res.json['id'] == userId
-	assert res.json['name'] == 'Tester'
+    # (*) Successfully edit user
+    url = testing('/api/user/{}'.format(userId), PUT)
+    res = app.put_json(url,
+                       {"name": user_name_edited,
+                        "email": user_email_edited,
+                        "password": user_password_edited},
+                       headers=auth)
+    res = app.get('/api/user/{}'.format(userId))  # make sure name is changed but id isn't
+    assert res.json['id'] == userId
+    assert res.json['name'] == user_name_edited
+    res = app.post_json('/api/user/login',  # make sure new credentials work for login
+                        {"email": user_email_edited, "password": user_password_edited})
+    assert res.json['id'] == userId
+    res = app.post_json('/api/user/login',
+                        {"email": user_email_edited, "password": user_password},
+                        status=401)
 
-	# (*) Password should not be visible
-	assert 'password' not in res.json
+    # (X) Unsuccessfully edit user, unauthorized
+    app.put_json(url, {"name": user_name}, status=401)
 
-	# (*) Auth token is correctly ignored, correct information
-	res = app.get(url, headers=auth)
-	standard_test(res)
-	assert res.json['id'] == userId
-	assert res.json['name'] == 'Tester'
+    # (X) Unsuccessfully edit user, invalid password
+    app.put_json(url, {"password": "abc"}, headers=auth, status=400)
 
-	# (X) User does not exist
-	url = testing('/api/user/{}'.format(5000), GET)
-	res = app.get(url, status=404)
-	url = testing('/api/user/{}'.format(-1), GET)
-	res = app.get(url, status=404)
+    # (-) Revert user edits
+    app.put_json(url,
+                 {"name": user_name,
+                  "email": user_email,
+                  "password": user_password},
+                 headers=auth)
 
-	# (*) Successfully edit user
-	url = testing('/api/user/{}'.format(userId), PUT)
-	res = app.put_json(url,
-		{"name": user_name_edited,
-		"email": user_email_edited,
-		"password": user_password_edited},
-		headers=auth)
-	res = app.get('/api/user/{}'.format(userId)) # make sure name is changed but id isn't
-	assert res.json['id'] == userId
-	assert res.json['name'] == user_name_edited
-	res = app.post_json('/api/user/login', # make sure new credentials work for login
-		{"email": user_email_edited, "password": user_password_edited})
-	assert res.json['id'] == userId
-	res = app.post_json('/api/user/login',
-		{"email": user_email_edited, "password": user_password},
-		status=401)
+    # PODCAST REGISTRATION #
+    url = testing('/api/podcast/new', POST)
 
-	# (X) Unsuccessfully edit user, unauthorized
-	app.put_json(url, {"name": user_name}, status=401)
-	
-	# (X) Unsuccessfully edit user, invalid password
-	app.put_json(url, {"password": "abc"}, headers=auth, status=400)
+    # (X) Leave out required info
+    app.post_json(url,
+                  {"title": "TestCast",
+                   "description": "Test description"},
+                  status=400)
+    app.post_json(url,
+                  {"link": podcast_link,
+                   "description": "Test description"},
+                  status=400)
+    app.post_json(url,
+                  {"link": podcast_link,
+                   "title": "TestCast"},
+                  status=400)
 
-	# (-) Revert user edits
-	app.put_json(url,
-		{"name": user_name,
-		"email": user_email,
-		"password": user_password},
-		headers=auth)
+    # (*) Successfully register
+    res = app.post_json(url,
+                        {"link": podcast_link,
+                         "title": "TestCast",
+                         "description": "Test description"})
+    podcastId = res.json['id']
 
-
-	### PODCAST REGISTRATION ###
-	url = testing('/api/podcast/new', POST)
-
-	# (X) Leave out required info
-	app.post_json(url,
-		{"title": "TestCast",
-		"description": "Test description"},
-		status=400)
-	app.post_json(url,
-		{"link": podcast_link,
-		"description": "Test description"},
-		status=400)
-	app.post_json(url,
-		{"link": podcast_link,
-		"title": "TestCast"},
-		status=400)
-
-	# (*) Successfully register
-	res = app.post_json(url,
-		{"link": podcast_link,
-		"title": "TestCast",
-		"description": "Test description"})
-	podcastId = res.json['id']
-
-	# (X) Podcast exists
-	app.post_json(url,
-		{"link": podcast_link,
-		"title": "TestCast",
-		"description": "Test description"},
-		status=409)
+    # (X) Podcast exists
+    app.post_json(url,
+                  {"link": podcast_link,
+                   "title": "TestCast",
+                   "description": "Test description"},
+                  status=409)
